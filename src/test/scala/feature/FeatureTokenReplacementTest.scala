31b0f31ee5ca79983491332c42708069c10cb8a1
d-smith <doug.smith.mail@gmail.com>
2013-11-01 08:28:32 -0600

    Added additional test case.

--- src/test/scala/feature/FeatureTokenReplacementTest.scala
+++ src/test/scala/feature/FeatureTokenReplacementTest.scala
@@ -10,6 +10,8 @@ class FeatureTokenReplacementTest extends FunSuite
   test("config sections can have properties replaced") {
     val propMap = propertyMapFromFile("src/test/resources/sample.properties")
     val updated = replaceTokenizedParamsFromMap(propMap, "src/test/resources/features.xml")
+    println(updated)
     assert(updated.contains("brokerURL  = vm://amq") === true)
+    assert(updated.contains("empty  = ") === true)
   }
 }
